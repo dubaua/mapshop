c08de483166318ef04679bc87d6b6b847f323e0c
Vladimir Lysov <dubaua@gmail.com>
2020-03-25 17:02:17 +0500

    add homepage styles

--- src/main.js
+++ src/main.js
@@ -2,15 +2,24 @@ import 'normalize.css';
 import 'swiper/css/swiper.min.css';
 import Swiper from 'swiper';
 
-var mySwiper = new Swiper('[data-showcase-swiper]', {
+var shortcutsSwiper = new Swiper('[data-shortcuts-swiper]', {
   slidesPerView: 'auto',
+  grabCursor: true,
+  navigation: {
+    nextEl: '[data-shortcuts-swiper-next]',
+    prevEl: '[data-shortcuts-swiper-prev]',
+  },
+  pagination: {
+    el: '[data-shortcuts-swiper-pagination]',
+    clickable: true,
+  },
+});
+
+var showcaseSwiper = new Swiper('[data-showcase-swiper]', {
+  slidesPerView: 3,
   grabCursor: true,
   navigation: {
     nextEl: '[data-showcase-swiper-next]',
     prevEl: '[data-showcase-swiper-prev]',
-  },
-  pagination: {
-    el: '[data-showcase-swiper-pagination]',
-    clickable: true,
   },
 });
