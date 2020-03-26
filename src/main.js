b0bbdcdb86c50d643890ee65b1a9880302e01570
Vladimir Lysov <dubaua@gmail.com>
2020-03-26 21:38:32 +0500

    add css vars, add mobile layout

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import 'swiper/css/swiper.min.css';
 import Swiper from 'swiper';
 
 var shortcutsSwiper = new Swiper('[data-shortcuts-swiper]', {
-  slidesPerView: 'auto',
+  slidesPerView: 1,
+  breakpoints: {
+    1200: {
+      slidesPerView: 'auto',
+    },
+  },
   grabCursor: true,
   navigation: {
     nextEl: '[data-shortcuts-swiper-next]',
@@ -16,7 +21,16 @@ var shortcutsSwiper = new Swiper('[data-shortcuts-swiper]', {
 });
 
 var showcaseSwiper = new Swiper('[data-showcase-swiper]', {
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
   grabCursor: true,
   navigation: {
     nextEl: '[data-showcase-swiper-next]',
